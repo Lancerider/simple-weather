3d564e472742e2c58c119945b2b7a8e56a790674
Jorge Luis Palacios <jorgepalacioscl@gmail.com>
2021-10-14 23:01:33 -0300

    :tada: initial commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <WheatherApp msg="Welcome to Your Vue.js App"/>
+  </div>
+</template>
+
+<script>
+import WheatherApp from './components/WheatherApp.vue';
+
+export default {
+  name: 'App',
+
+  components: {
+    WheatherApp,
+  },
+
+  mounted() {
+    console.log(process.env);
+  },
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ src/components/WheatherApp.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div class="app-header">
+      <div class="header__title">
+        Simple Wheater
+      </div>
+      <div class="header_search-icon"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'WheatherApp',
+
+  data() {
+    return {
+      cities: ['RIO DE JANEIRO, BEIJING, LOS ANGELES'],
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+</style>
